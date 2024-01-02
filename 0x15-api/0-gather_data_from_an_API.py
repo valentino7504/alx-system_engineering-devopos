d251ef028d4fa3bba8d9d4ddaa9dc4ef7108e9a8
Edwin Ade <edwinmbonyjr@gmail.com>
2024-01-02 14:49:33 +0000

    Added main guard

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,28 +5,25 @@ It gets the todo list of an employee with a specific ID
 The ID is accepted via argv
 Requests are made using the requests module
 """
-
-
 import requests
 import sys
-
-
-employee_id = sys.argv[1]
-url = "https://jsonplaceholder.typicode.com/users/"
-employee_details = requests.get(
-    f"{url}{employee_id}"
-).json()
-employee_name = employee_details.get("name")
-todos = requests.get(
-    f"{url}{employee_id}/todos"
-).json()
-completed = 0
-task_titles = []
-for task in todos:
-    if task.get("completed"):
-        completed += 1
-        task_titles.append(task.get("title"))
-print(f"Employee {employee_name} is done with tasks" +
-      f"({completed}/{len(todos)}):")
-for title in task_titles:
-    print(f"\t {title}")
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/"
+    employee_details = requests.get(
+        f"{url}{employee_id}"
+    ).json()
+    employee_name = employee_details.get("name")
+    todos = requests.get(
+        f"{url}{employee_id}/todos"
+    ).json()
+    completed = 0
+    task_titles = []
+    for task in todos:
+        if task.get("completed"):
+            completed += 1
+            task_titles.append(task.get("title"))
+    print(f"Employee {employee_name} is done with tasks" +
+          f"({completed}/{len(todos)}):")
+    for title in task_titles:
+        print(f"\t {title}")
