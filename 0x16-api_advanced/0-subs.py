8a1dd1d3c945568aa799d9004cfe744f94ce6494
Edwin Ade <edwinmbonyjr@gmail.com>
2024-08-18 00:01:07 +0100

    Fix: Solution for task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -22,8 +22,6 @@ def number_of_subscribers(subreddit):
         "Chrome/127.0.0.0 Safari/537.36"}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
-    response = response.json()
-    try:
-        return response.get("data").get("subscribers")
-    except Exception:
+    if response.status_code != 200:
         return 0
+    return response.json().get("data").get("subscribers")
